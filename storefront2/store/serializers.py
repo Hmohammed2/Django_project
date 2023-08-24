3b0919d4a5de43a22d3d9a141bd455b6c2db0a79
Hamza <Hamza_mohammed15@hotmail.com>
2023-08-24 06:31:37 +0100

    modified serializers

---
+++ storefront2/store/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from decimal import Decimal
+from . import models
+
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Collection
+        fields = ['id', 'title']
+
+class ProductSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = models.Product
+        fields = ['id', 'title', 'description', 'slug', 'inventory', 'unit_price', 'price_with_tax', 'collection']
+
+    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+
+    
+    def calculate_tax(self, product: models.Product):
+        return product.unit_price * Decimal(1.10)
--- storefront2/store/views.py
+++ storefront2/store/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Product, Collection
+from .serializers import ProductSerializer, CollectionSerializer
 
 # Create your views here.
+@api_view(['GET', 'POST'])
+def product_list(request):
+    if request.method == "GET":
+        queryset = Product.objects.select_related('collection').all()
+        serializer = ProductSerializer(queryset, many=True, context={'request': request})
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        print(serializer.validated_data)
+        return Response('ok')
+
+
+@api_view()
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+@api_view()
+def collection_detail(request, pk):
+    collection = get_object_or_404(Collection, pk=pk)
+    serializer = CollectionSerializer(collection)
+    return Response(serializer.data)
